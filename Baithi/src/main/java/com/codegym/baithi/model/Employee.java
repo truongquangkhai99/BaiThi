0b8665cea7d93f51ff75a067f8caf633f0ee87af
unknown <volong24071995>
2020-11-25 10:21:37 +0700

    update

--- Baithi/src/main/java/com/codegym/baithi/model/Employee.java
+++ Baithi/src/main/java/com/codegym/baithi/model/Employee.java
@@ -25,8 +25,10 @@ public class Employee {
     @Size(min = 2,max = 40,message = "Tên tối thiểu 2 kí tự tối đa 40 kí tự")
     private String name;
 
+    @NotNull(message = "Không để trống")
     private Date dob;
-    @NotNull
+
+    @NotNull(message = "Không để trống")
     private String gender;
 
     @NotBlank(message = "Không để trống")
@@ -35,6 +37,7 @@ public class Employee {
     private String phone;
 
     @NotBlank(message = "Không để trống")
+    @Size(min = 9,max=9,message = "Số chứng minh gồm 9 số")
     private String cmnd;
 
     @Email(regexp = "^[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}$")
